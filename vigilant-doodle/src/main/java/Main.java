408ee51b1802d48fbd74430dbd3436392c111466
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-01-04 16:53:20 +0200

    Lisätty MongoDB driver

--- vigilant-doodle/src/main/java/Main.java
+++ vigilant-doodle/src/main/java/Main.java
@@ -1,5 +1,7 @@
 package logiikka;
 
+import config.Configuration;
+
 /**
  * Created by kuujakke on 22.12.2016.
  */
@@ -10,9 +12,26 @@ public class Main {
         Task task = new Task("TestTask");
         Supervisor supervisor = new Supervisor(user, task);
         System.out.println(supervisor);
+
+        Configuration conf = new Configuration();
+        conf.setProjectName("New Project");
+        conf.setProjectDescription("No description yet.");
+        conf.setTaskName("New Task");
+        conf.setTaskDescription("No description yet.");
+        conf.setJobName("New Job");
+        conf.setJobDescription("No description yet.");
+        conf.setUserName("DefaultUser");
+        conf.setUserPassword("password1");
+        conf.setDBUser("TestUser");
+        conf.setDBPassword("TestPassword");
+        conf.setDBName("vigilant-doodle");
+        conf.writeProperties();
+        System.out.println(conf);
+
     }
     public static void testi(int i) {
         System.out.println(i);
     }
+
 }
 
